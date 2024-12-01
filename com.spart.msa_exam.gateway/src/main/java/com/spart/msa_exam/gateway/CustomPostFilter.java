888962e3d1e4635c882c56a9a98ee85a60f69f2b
Griotold <gotjd9773@naver.com>
2024-12-01 18:26:09 +0900

    [common] Server-Port 추가

--- com.spart.msa_exam.gateway/src/main/java/com/spart/msa_exam/gateway/CustomPostFilter.java
+++ com.spart.msa_exam.gateway/src/main/java/com/spart/msa_exam/gateway/CustomPostFilter.java
@@ -1,29 +1,40 @@
 package com.spart.msa_exam.gateway;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
 @Slf4j
 @Component
 public class CustomPostFilter implements GlobalFilter, Ordered {
 
+    private static final String SERVER_PORT_HEADER = "Server-Port";
+
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> addServerPortInHeader(exchange)));
+    }
+
+    private void addServerPortInHeader(ServerWebExchange exchange) {
+        URI uri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+
+        if (uri != null) {
             ServerHttpResponse response = exchange.getResponse();
-            log.info("Post Filter: Response status code: {}", response.getStatusCode());
-            // todo Server-Port
-//            Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
-//            URI uri = route.getUri();
-//            String port = String.valueOf(uri.getPort());
-//            exchange.getResponse().getHeaders().add("Server-Port", port);
-//            log.info("Post Filter: Server Port: {}", port);
-        }));
+            HttpHeaders headers = response.getHeaders();
+
+            String port = String.valueOf(uri.getPort());
+            headers.add(SERVER_PORT_HEADER, port);
+        }
     }
 
     @Override
