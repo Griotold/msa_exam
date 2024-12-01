1995b81a53d3ae2939241c246aa287096071e667
Griotold <gotjd9773@naver.com>
2024-12-01 12:51:55 +0900

    [auth] JwtTokenProviderTest
    1. `@SpringBootTest` -> @ExtendWith(MockitoExtentions.class)
    2. JwtTokenProvider는 토큰을 만들어주는 역할만 진행하므로 불필요한 스프링 환경을 띄울 필요가 없다.
    3. ReflectionTestUtils 를 사용하면 `@Value`로 주입 받는 값들을 지정해줄 수 있다.

--- com.spart.msa_exam.auth/src/test/java/com/spart/msa_exam/auth/infra/config/jwt/JwtTokenProviderTest.java
+++ com.spart.msa_exam.auth/src/test/java/com/spart/msa_exam/auth/infra/config/jwt/JwtTokenProviderTest.java
@@ -3,20 +3,38 @@ package com.spart.msa_exam.auth.infra.config.jwt;
 import com.spart.msa_exam.auth.domain.enums.Role;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.crypto.SecretKey;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest
-@ActiveProfiles("test")
+/**
+ * @Value로 주입받는 값들은 ReflectionTestUtils를 사용해 직접 설정할 수 있습니다
+ * */
+@ExtendWith(MockitoExtension.class)
 class JwtTokenProviderTest {
 
-    @Autowired
     private JwtTokenProvider jwtTokenProvider;
+
+    @BeforeEach
+    void setUp() {
+        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        String encodedKey = Encoders.BASE64URL.encode(secretKey.getEncoded());
+        jwtTokenProvider = new JwtTokenProvider(encodedKey);
+        ReflectionTestUtils.setField(jwtTokenProvider, "issuer", "auth-service");
+        ReflectionTestUtils.setField(jwtTokenProvider, "accessExpiration", 3600000L);
+    }
 
     @Test
     @DisplayName("JWT 토큰 생성 테스트")
