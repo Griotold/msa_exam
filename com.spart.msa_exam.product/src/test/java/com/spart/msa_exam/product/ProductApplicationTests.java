b7ff8be5c0339ea57f7b607a3cd708b15631e44d
Griotold <gotjd9773@naver.com>
2024-12-02 12:05:01 +0900

    [product] build.gradle 및 application.yml 파일들

--- com.spart.msa_exam.product/src/test/java/com/spart/msa_exam/product/ProductApplicationTests.java
+++ com.spart.msa_exam.product/src/test/java/com/spart/msa_exam/product/ProductApplicationTests.java
@@ -2,7 +2,9 @@ package com.spart.msa_exam.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ProductApplicationTests {
 
