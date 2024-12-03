d22e1e78906a431142d6e8bb1ef00d7d7a44b40c
Griotold <gotjd9773@naver.com>
2024-12-03 15:22:31 +0900

    [cache] 주문 단건 조회에 캐시 적용하기 - Cache-Aside

--- com.spart.msa_exam.order/src/main/java/com/spart/msa_exam/order/application/usecase/OrderReadUseCase.java
+++ com.spart.msa_exam.order/src/main/java/com/spart/msa_exam/order/application/usecase/OrderReadUseCase.java
@@ -5,6 +5,7 @@ import com.spart.msa_exam.order.domain.service.OrderService;
 import com.spart.msa_exam.order.domain.service.dto.OrderProductResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class OrderReadUseCase {
 
     private final OrderService orderService;
 
+    @Cacheable(cacheNames = "orderCache", key = "#command.id")
     public List<OrderProductResponse> execute(OrderReadCommand command) {
         log.info("OrderReadUseCase.command : {}", command);
         return orderService.getOrderProducts(command.id());
--- com.spart.msa_exam.order/src/main/java/com/spart/msa_exam/order/domain/service/dto/OrderProductResponse.java
+++ com.spart.msa_exam.order/src/main/java/com/spart/msa_exam/order/domain/service/dto/OrderProductResponse.java
@@ -2,11 +2,13 @@ package com.spart.msa_exam.order.domain.service.dto;
 
 import com.spart.msa_exam.order.domain.entity.OrderProduct;
 
+import java.io.Serializable;
+
 public record OrderProductResponse(
         Long orderProductId,
         Long orderId,
         Long productId
-) {
+) implements Serializable {
 
     public static OrderProductResponse from(OrderProduct orderProduct) {
         return new OrderProductResponse(
