2a6a2719f5de57f5167bed94d563ec8b97e878ac
Griotold <gotjd9773@naver.com>
2024-12-03 15:49:03 +0900

    [cache] 상품 목록 조회 캐시 적용 - Cache-Aside

--- com.spart.msa_exam.product/src/main/java/com/spart/msa_exam/product/domain/service/dto/ProductResponse.java
+++ com.spart.msa_exam.product/src/main/java/com/spart/msa_exam/product/domain/service/dto/ProductResponse.java
@@ -2,8 +2,11 @@ package com.spart.msa_exam.product.domain.service.dto;
 
 import com.spart.msa_exam.product.domain.entity.Product;
 
-public record ProductResponse(Long id, String name, Integer supplyPrice) {
+import java.io.Serializable;
 
+public record ProductResponse(Long id, String name, Integer supplyPrice)
+
+        implements Serializable {
     public static ProductResponse from(Product product) {
         return new ProductResponse(product.getId(), product.getName(), product.getSupplyPrice());
     }
